16bd19a99a89560da903b8d3b59f2ce0291a2f2a
mmnikiforov <mmnikiforov@edu.tversu.ru>
2023-03-29 13:53:11 +0300

    init commit

---
+++ lab_4/ex_1.py
@@ -0,0 +1,18 @@
+def sort_viborom(arr):
+    sorting_steps = []
+    len_arr = len(arr)
+    for i in range(len_arr - 1):
+        j = i
+        for index in range(i + 1, len_arr):
+            if arr[index] < arr[j]:
+                j = index
+        arr[i], arr[j] = arr[j], arr[i]
+        sorting_steps.append(list(arr))
+    return sorting_steps
+
+
+array = input().split(' ')
+arr = [int(i) for i in array]
+sorting_steps = sort_viborom(arr)
+for steps in sorting_steps:
+    print(' '.join(str(i) for i in steps))
