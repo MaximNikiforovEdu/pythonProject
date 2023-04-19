b4b9230ccd64f5c7a487c44fe390cde02bcd3974
MaximNikiforovEdu <mmnikiforov@edu.tversu.ru>
2023-04-19 13:13:51 +0300

    init commit

---
+++ lab_4/ex_3.py
@@ -0,0 +1,16 @@
+def sort_mini_puzir(arr):
+    sorting_steps = []
+    len_arr = len(arr)
+    for i in range(len_arr - 1):
+        for j in range(len_arr - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        sorting_steps.append(list(arr))
+    return sorting_steps
+
+
+array = input().split(' ')
+arr = [int(i) for i in array]
+sorting_steps = sort_mini_puzir(arr)
+for steps in sorting_steps:
+    print(' '.join(str(i) for i in steps))
