fa3af4a4872a286b9901670cdd49824e1ddffc91
MaximNikiforovEdu <mmnikiforov@edu.tversu.ru>
2023-04-19 13:15:13 +0300

    init commit

---
+++ lab_4/ex_4.py
@@ -0,0 +1,20 @@
+def sort_puzir(arr):
+    sorting_steps = []
+    len_arr = len(arr)
+    for i in range(len_arr - 1):
+        ok = True
+        for j in range(len_arr - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                ok = False
+        sorting_steps.append(list(arr))
+        if ok == True:
+            return sorting_steps
+    return sorting_steps
+
+
+array = input().split(' ')
+arr = [int(i) for i in array]
+sorting_steps = sort_puzir(arr)
+for steps in sorting_steps:
+    print(' '.join(str(i) for i in steps))
