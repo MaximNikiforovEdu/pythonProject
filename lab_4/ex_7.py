13fa8d001ed8e915cf705fee2dfb233ccb5d9c01
MaximNikiforovEdu <mmnikiforov@edu.tversu.ru>
2023-04-19 13:17:50 +0300

    init commit

---
+++ lab_4/ex_7.py
@@ -0,0 +1,15 @@
+def sort_podschet(arr):
+    k = max(arr)
+    chast = [0 for i in range(k + 1)]
+    len_arr = len(arr)
+    rez = []
+    for i in arr:
+        chast[i] += 1
+    for i in range(k + 1):
+        for j in range(chast[i]):
+            rez.append(str(i))
+    return rez
+
+arr = input().split(' ')
+arr = [int(i) for i in arr]
+print(' '.join(sort_podschet(arr)))
