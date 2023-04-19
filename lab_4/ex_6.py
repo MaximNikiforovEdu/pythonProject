7e3482291e80274b06f63a621b233334d99c2265
MaximNikiforovEdu <mmnikiforov@edu.tversu.ru>
2023-04-19 13:17:16 +0300

    init commit

---
+++ lab_4/ex_6.py
@@ -0,0 +1,21 @@
+def sort_shella(arr, h):
+    sorting_steps = []
+    len_arr = len(arr)
+    for h_elem in h:
+        for i in range(h_elem, len_arr):
+            elem = arr[i]
+            index = i
+            while(index >= h_elem and elem < arr[index - h_elem]):
+                arr[index] = arr[index - h_elem]
+                index -= h_elem
+            arr[index] = elem
+        sorting_steps.append(list(arr))
+    return sorting_steps
+
+h = input().split(' ')
+h = [int(i) for i in h]
+arr = input().split(' ')
+arr = [int(i) for i in arr]
+sorting_steps = sort_shella(arr, h)
+for steps in sorting_steps:
+    print(' '.join(str(i) for i in steps))
