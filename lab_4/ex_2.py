c4fa5f903d55dc565a54f15b0c9272dd9183b707
MaximNikiforovEdu <mmnikiforov@edu.tversu.ru>
2023-04-19 13:11:19 +0300

    init commit

---
+++ lab_4/ex_2.py
@@ -0,0 +1,19 @@
+def sort_vstavkami(arr):
+    sorting_steps = []
+    len_arr = len(arr)
+    for i in range(1, len_arr):
+        elem = arr[i]
+        index = i
+        while index > 0 and elem < arr[index - 1]:
+            arr[index] = arr[index - 1]
+            index -= 1
+        arr[index] = elem
+        sorting_steps.append(list(arr))
+    return sorting_steps
+
+
+array = input().split(' ')
+arr = [int(i) for i in array]
+sorting_steps = sort_vstavkami(arr)
+for steps in sorting_steps:
+    print(' '.join(str(i) for i in steps))
